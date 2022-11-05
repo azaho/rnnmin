930743b0b30387c3ef7535ab0f2bad094c7b61ed
Andrii Zahorodnii <zaho@mit.edu>
2022-11-05 03:16:12 -0400

    add results

--- C1.py
+++ C1.py
@@ -1491,3 +1491,12 @@ merger.close()
 
 
 
+# FOR OPENMIND
+import pathlib
+_path = pathlib.Path(f"results/{index}_{directory}/file.png")
+_path.parent.mkdir(parents=True, exist_ok=True)
+merger.write(f"results/{index}_{directory}/RESULT.pdf")
+dirname = f"PCA_{activity_of}_fixed"
+os.system(f"ffmpeg -framerate 10 -y -pattern_type glob -i '{directory}/{index}/{dirname}/*.png' -c:v libx264 -pix_fmt yuv420p results/{index}_{directory}/{dirname}.mp4")
+dirname = f"PCA_{activity_of}_2d_fixed"
+os.system(f"ffmpeg -framerate 10 -y -pattern_type glob -i '{directory}/{index}/{dirname}/*.png' -c:v libx264 -pix_fmt yuv420p results/{index}_{directory}/{dirname}.mp4")
